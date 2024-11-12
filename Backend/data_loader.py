7492c134570c1ba232b9b6f2caa46c2599281907
justin00195 <120707073+justin00195@users.noreply.github.com>
2024-11-11 16:21:06 -0800

    os.path

--- Backend/data_loader.py
+++ Backend/data_loader.py
@@ -3,13 +3,18 @@ import numpy as np
 import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
+import scipy.io
 
-BASEPATH = "/Users/chinghaochang/project-170/data/images/"  # image's path
+
+BASEPATH = os.path.join("data", "images")  
+LISTS_PATH = os.path.join("data", "lists", "test_list.mat")  
 
 def load_and_preprocess_image(path):
+    # Read and preprocess a single image: resize and convert color space
     image = cv2.imread(path)
-    image = cv2.resize(image, (224, 224))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    if image is not None:  
+        image = cv2.resize(image, (224, 224))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     return image
 
 # use "train_test_split" to random split load_data function 
@@ -18,14 +23,15 @@ def load_data_with_split():
     paths = []
     for d in os.listdir(BASEPATH):
         LABELS.add(d)
-        paths.append((BASEPATH + d, d))
+        paths.append((os.path.join(BASEPATH, d), d))
     
     X, y = [], []
     for path, label in paths:
         for image_path in os.listdir(path):
             image = load_and_preprocess_image(os.path.join(path, image_path))
-            X.append(image)
-            y.append(label)
+            if image is not None:
+                X.append(image)
+                y.append(label)
     
     encoder = LabelBinarizer()
     X = np.array(X)
@@ -37,8 +43,7 @@ def load_data_with_split():
 
 # use .mat to split load_data function
 def load_data_from_mat():
-    import scipy.io
-    mat_data = scipy.io.loadmat("/Users/chinghaochang/project-170/data/lists/test_list.mat")
+    mat_data = scipy.io.loadmat(LISTS_PATH)
 
     file_list = [item[0][0] for item in mat_data['file_list']]
     labels = [int(label[0]) for label in mat_data['labels']]
@@ -46,9 +51,8 @@ def load_data_from_mat():
     images = []
     for file_path in file_list:
         full_path = os.path.join(BASEPATH, file_path)
-        image = cv2.imread(full_path)
+        image = load_and_preprocess_image(full_path)
         if image is not None:
-            image = cv2.resize(image, (224, 224))
             images.append(image)
         else:
             print(f"Warning: Image at {full_path} could not be loaded.")
