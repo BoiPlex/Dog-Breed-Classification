462e331dc72c8eaee2de274cfc6294e7383a221a
justin00195 <120707073+justin00195@users.noreply.github.com>
2024-11-28 20:20:05 -0800

    Update model.py

--- Backend/model.py
+++ Backend/model.py
@@ -1,20 +1,18 @@
+
 from tensorflow.keras.applications import VGG16
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
 
 def create_model(num_classes):
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-    for layer in base_model.layers[:-5]:
+
+    for layer in base_model.layers:
         layer.trainable = False
-    for layer in base_model.layers[-5:]:
-        layer.trainable = True
 
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1024, activation='relu')(x)
-    x = Dense(1024, activation='relu')(x)
-    x = Dropout(0.5)(x)
     x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
     preds = Dense(num_classes, activation='softmax')(x)
 
     model = Model(inputs=base_model.input, outputs=preds)
