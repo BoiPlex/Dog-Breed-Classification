48ffe6491bd298a107f84f6ddfcfc063f36f3af9
justin00195 <120707073+justin00195@users.noreply.github.com>
2024-12-08 20:34:00 -0800

    Update model.py

--- Backend/model.py
+++ Backend/model.py
@@ -5,13 +5,17 @@ from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
 def create_model(num_classes):
     base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
-    for layer in base_model.layers:
+    for layer in base_model.layers[:15]:
         layer.trainable = False
+    for layer in base_model.layers[15:]:
+        layer.trainable = True
 
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(512, activation='relu')(x)
-    x = Dropout(0.5)(x)
+    x=Dense(1024,activation='relu')(x)
+    x=Dense(1024,activation='relu')(x)
+    x=Dropout(0.5)(x)
+    x=Dense(512,activation='relu')(x)
     preds = Dense(num_classes, activation='softmax')(x)
 
     model = Model(inputs=base_model.input, outputs=preds)
