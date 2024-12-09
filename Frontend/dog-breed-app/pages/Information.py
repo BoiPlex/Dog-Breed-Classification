591c1f40a33c5bb845534b073f1342fabbefc237
Qiyu Lu <88757657+qiyulu437@users.noreply.github.com>
2024-12-08 21:42:47 -0800

    Update Information.py

--- Frontend/dog-breed-app/pages/Information.py
+++ Frontend/dog-breed-app/pages/Information.py
@@ -3,7 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Information")
 
 st.header("Project Information")
-st.subheader("Overview")
+st.markdown("""
+##Overview
+""")
 
 st.markdown("""
 This project focuses on **dog breed classification** using the **VGG16 model**, aiming to accurately identify dog breeds from images. We fine-tuned a VGG16 model and trained it with the [**Stanford Dog Breed dataset**](http://vision.stanford.edu/aditya86/ImageNetDogs/) to make it suitable for classifying specific breeds.
