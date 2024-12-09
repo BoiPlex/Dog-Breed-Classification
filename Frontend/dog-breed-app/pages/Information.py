b54301bb5cf80a4a08c7bb51051afd2fe8beb3a8
Qiyu Lu <88757657+qiyulu437@users.noreply.github.com>
2024-12-08 21:41:54 -0800

    Update Information.py

--- Frontend/dog-breed-app/pages/Information.py
+++ Frontend/dog-breed-app/pages/Information.py
@@ -11,6 +11,94 @@ This project focuses on **dog breed classification** using the **VGG16 model**, 
 Additionally, we compared the performance of three optimizers: **Adam**, **AdamW**, and **SGD**, on this large-scale classification task. By analyzing the results, we gained insights into how different optimization strategies affect the **performance and efficiency** of the model in actual image classification tasks.
 """)
 
+st.markdown("""
+## Model:
+
+  
+
+Use VGG16 as the base model, preload the weights to ImageNet, and remove the original classification layer. We make modifications to the original model.
+
+First 15 layers freeze: Fix the weights of early feature extraction layers to avoid overfitting.
+
+Layer 15 after unfreezing: allows deep weight updates to improve the model's adaptability to the current data set.
+
+  
+
+## Train strategy:
+
+  
+
+Data Augmentation:
+
+  
+
+Use ImageDataGenerator to enhance training data online. Methods include:
+
+  
+
+```python  
+  
+datagen = ImageDataGenerator(
+
+rotation_range=20,
+
+width_shift_range=0.2,
+
+height_shift_range=0.2,
+
+zoom_range=0.2,
+
+horizontal_flip=True,
+
+fill_mode='nearest'
+
+)
+
+  
+```
+
+  
+  
+
+## Learning Rate Scheduler:
+
+  
+
+ReduceLROnPlateau:
+
+  
+
+When the validation loss shows no improvement in 10 epochs, cut the learning rate in half. The minimum learning rate is set to 1e-6.
+
+  
+
+LearningRateLogger:
+
+Record and print the learning rate changes for each epoch.
+
+  
+
+Early Stopping:
+
+  
+
+Monitor validation loss (val_loss).
+
+If the validation loss does not improve after 30 epochs, terminate training and restore the optimal weights.
+
+  
+
+Training model:
+
+  
+
+Batch size: 128
+
+Maximum number of Epochs: 500
+
+We use more than expected Epochs as set up because we purchased colab pro+ and there still is a limit for compute usage.
+""")
+
 
 st.image("Frontend/dog-breed-app/src/adamw_metrics.png", use_column_width=True)
 st.image("Frontend/dog-breed-app/src/adam_metrics.png", use_column_width=True)
